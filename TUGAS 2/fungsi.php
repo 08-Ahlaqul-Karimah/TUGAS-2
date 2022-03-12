2a16dd4307c8628c9561b8274957c60838a45d6b
08-ahlaqul-karimah <ahlaqulkarimah08@gmail.com>
2022-03-12 20:48:26 +0700

    tugas 2 menambahkan file fungsi.php

---
+++ TUGAS 2/fungsi.php
@@ -0,0 +1,23 @@
+<?php
+//fungsi pertama
+    function hello (){
+        return "AHLAQUL KARIMAH M";
+    }
+//fungsi kedua   
+    function nim (){
+        return "200411100008";
+    }
+//fungsi ketiga
+    function alamat (){
+        return "desa trepan, kecamatan Babat, kabupaten Lamongan";
+    }
+
+#fungsi pertama 
+    echo ("AHLAQUL KARIMAH M");
+    echo "<br>" ;
+#fungsi kedua
+    echo ("200411100008");
+    echo "<br>" ;
+#fungsi ketiga
+    echo ("desa trepan, kecamatan Babat, kabupaten Lamongan");
+?>
