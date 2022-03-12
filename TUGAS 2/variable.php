c5dbaa3eb3284aafc34717ca2fbc9466b3abb8f5
08-ahlaqul-karimah <ahlaqulkarimah08@gmail.com>
2022-03-12 20:37:43 +0700

    tugas 2 menambahkan file variable.php

---
+++ TUGAS 2/variable.php
@@ -0,0 +1,19 @@
+<?php
+    echo ("OPERASI PENJUMLAHAN :");
+    echo "<br>";
+    $x = 3;
+    $y = 2;
+    $hasil_penjumlahan = $x+$y;
+    $hasil_pengurangan = $x-$y;
+    $hasil_perkalian = $x*$y;
+
+    echo "$x + $y = $hasil_penjumlahan" ;
+    echo"<br>";
+    echo ("HASIL PENGURANGAN :");
+    echo"<br>";
+    echo "$x - $y = $hasil_pengurangan" ;
+    echo"<br>";
+    echo ("HASIL PERKALIAN :");
+    echo"<br>";
+    echo "$x * $y = $hasil_perkalian" ;
+?>
